f28dce4c812620af034006f8191f747407e36b9b
Tim De Jong <gitlab@thinkpractice.nl>
2025-12-10 16:31:09 +0100

    Download models

--- Sources/swift-hugging-face-cli/SwiftHuggingFaceCli.swift
+++ Sources/swift-hugging-face-cli/SwiftHuggingFaceCli.swift
@@ -5,6 +5,7 @@
 // https://swiftpackageindex.com/apple/swift-argument-parser/documentation
 
 import ArgumentParser
+import Foundation
 import HuggingFace
 
 @main
@@ -15,6 +16,32 @@ struct SwiftHuggingFaceCli: AsyncParsableCommand {
 
     @Option(name: .shortAndLong, help: "Number of results to return")
     var numberOfResults: Int = 10
+
+    @Option(name: .shortAndLong, help: "Destination path for model")
+    var destinationPath: String = "./models"
+
+    func downloadFile(client: HubClient, repo: Repo.ID, file: Git.TreeEntry, to destination: String)
+        async throws
+    {
+        let destination = FileManager.default.temporaryDirectory
+            .appendingPathComponent("model.safetensors")
+
+        // Download with progress tracking
+        let progress = Progress(totalUnitCount: 0)
+        /*Task {
+            for await _ in progress.values(forKeyPath: \.fractionCompleted) {
+                print("Download progress: \(progress.fractionCompleted * 100)%")
+            }
+        }*/
+
+        let fileURL = try await client.downloadFile(
+            at: file.path,
+            from: repo,
+            to: destination,
+            progress: progress
+        )
+        print("Downloaded file url: \(fileURL.absoluteString)")
+    }
 
     mutating func run() async throws {
         let client = HubClient.default
@@ -36,12 +63,27 @@ struct SwiftHuggingFaceCli: AsyncParsableCommand {
         print("Downloads: \(model.downloads ?? 0)")
         print("Likes: \(model.likes ?? 0)")
 
+        let modelDirectory = URL(filePath: destinationPath).appending(path: "\(model.id)")
+
         // Get model tags
         do {
             let tags = try await client.getModelTags()
             print("Tags \(tags.map { "\($0.key):\($0.value)" }.joined(separator: ","))")
         } catch {
             print("Error retrieving tags:\n \(error)")
+        }
+
+        let files = try await client.listFiles(
+            in: model.id, kind: .model, revision: "main", recursive: true)
+        for file in files {
+            print(file.path)
+            if file.type == .file {
+                print("Downloading: \(file.path)")
+                let filePath = modelDirectory.appending(path: file.path)
+                try await downloadFile(
+                    client: client, repo: firstModel.id, file: file, to: filePath.absoluteString)
+                print("Ready")
+            }
         }
     }
 }
